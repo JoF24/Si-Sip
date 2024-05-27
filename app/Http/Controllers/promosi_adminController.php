953ffb6d5d1ab3c116c580af026bd6f3ee76d036
JoF24 <ferdi24joe@gmail.com>
2024-05-27 07:06:13 +0700

    Tambah Fitur Promosi Admin

---
+++ app/Http/Controllers/promosi_adminController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\promosi;
+use Illuminate\Http\Request;
+
+class promosi_adminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        if ($request->session()->has('user')) {
+            // Jika ada data pengguna dalam session, ambil informasi pengguna
+            $user = $request->session()->get('user');
+            $promosi = promosi::all();
+            return view('promosi_admin', compact('user','promosi'));
+        } else {
+            // Jika tidak ada data pengguna dalam session, redirect ke halaman login
+            return redirect()->route('login')->with('error', 'Silakan login terlebih dahulu.');
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
